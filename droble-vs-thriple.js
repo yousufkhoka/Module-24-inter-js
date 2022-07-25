d4e9ecac3b2646e77cb83e581e9fd3dba0014533
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2022-07-25 09:52:40 -0700

    droble vs thriple interview

---
+++ droble-vs-thriple.js
@@ -0,0 +1,17 @@
+const num1 = 3;
+const num2 = '3';
+if(num1 == num2){
+    console.log('condetion is true')
+}
+else{
+    console.log('condetion is false')
+}
+
+const num3 = 3;
+const num4 = '3';
+if(num1 === num2){
+    console.log('condetion is true')
+}
+else{
+    console.log('condetion is false')
+}
