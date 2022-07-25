b863c03f2cec2796fc1ea983584e662afcdd7ebb
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2022-07-25 05:21:01 -0700

    ture vs false

---
+++ README.md
@@ -0,0 +1,1 @@
+# Module-24-inter-js
---
+++ ture-false.js
@@ -0,0 +1,49 @@
+// falsey value
+// 0
+// undefined
+// NaN
+// ""
+// false
+// null
+
+// truey value
+// '0'
+// true
+// " "
+// []
+
+let nam = "";
+if( nam){
+   console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+let num = 0
+if( nam){
+    console.log("condition is true")
+ }
+ else{
+     console.log("condition is false")
+ }
+
+
+ let arr = []
+ if( arr){
+    console.log("condition is true")
+ }
+ else{
+     console.log("condition is false")
+ }
+
+ let starting = ' '
+ if( starting){
+    console.log("condition is true")
+ }
+ else{
+     console.log("condition is false")
+ }
+ 
+
+
