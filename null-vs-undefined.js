9a21f7dbc9c22bcc25e0cf24665b13b35d829f24
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2022-07-25 06:08:33 -0700

    null vs undefiend done for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let number;
+
+function add(num1,num2){
+    console.log(num1+num2)
+}
+const result = add(2,3)
+
+function add(num1 , num2){
+    console.log(num2)
+}
+add(2)
+function add(num1 , num2){
+    console.log(num1,num2)
+}
+add(2)
+
+const prason = {name:'Rifat', age:19, phone:'01983723934',}
+const {gfneme} = prason
+const age = [10,20,30,]
+console.log(age[4])
