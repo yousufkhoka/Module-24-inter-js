b45c26982f66b5de6f60ff1e861a191f0c8a8fc6
Your Namei git config --global user.name Your <yousuf8169@gmail.com>
2022-07-26 11:01:31 -0700

    slosure js interview

---
+++ map-filter.js
@@ -0,0 +1,24 @@
+const numbers = [ 3, 4, 5, 7, 9, 19, ] 
+// const output = []
+// for(let i = 0 ; i < numbers.length; i++){
+//      const eliment = numbers[i]
+//      const result = eliment * eliment
+//      output.push(result) 
+// }
+// function squear(eliment){
+//    return eliment * eliment
+// }
+// const isthere = numbers.map(squear)
+// const result = numbers.map(eliment => eliment * eliment)
+
+// const result = numbers.filter(x => x % 2 === 0)
+// const result = numbers.find(x => x > 4)
+
+
+const fruits = ["apple", "orange", "cherry"];
+ fruits.forEach(myFunction);
+ 
+function myFunction(item) {
+ return item 
+}
+
